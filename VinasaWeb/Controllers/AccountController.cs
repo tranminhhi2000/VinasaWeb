40f4a08dbb35853dabccdf2bb47b2669f8a898ee
tranminhhi <tranminhhi2732000@gmail.com>
2022-06-18 01:19:17 +0700

    Update Authorization Login

--- VinasaWeb/Controllers/AccountController.cs
+++ VinasaWeb/Controllers/AccountController.cs
@@ -392,7 +392,7 @@ namespace VinasaWeb.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Login", "Account");
         }
 
         //
