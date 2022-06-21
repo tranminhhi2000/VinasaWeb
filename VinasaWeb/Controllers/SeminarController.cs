2ab711767e13276860e3f577902846b27a425ef7
tienbap12 <tienbap12@gmail.com>
2022-06-21 12:25:18 +0700

    update seminar

---
+++ VinasaWeb/Controllers/SeminarController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VinasaWeb.Controllers
+{
+    public class SeminarController : Controller
+    {
+        // GET: Seminar
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
